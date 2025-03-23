c08d9a375b20b80d6098c83e17505e1fd5963e50
vikash-mdh <vikashkumar555550@gmail.com>
2025-03-23 15:52:29 +0530

    add new file test

---
+++ script.js
@@ -0,0 +1,45 @@
+
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperbyMap() {
+  arr.map(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
+}
+
+function PrintDeveloperbyForEach() {
+  arr.forEach(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
+}
+
+function addData() {
+  let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(arr);
+}
+
+function removeAdmin() {
+  arr = arr.filter(employee => employee.profession !== "admin");
+  console.log(arr);
+}
+
+function concatenateArray() {
+  let newArr = [
+    { id: 5, name: "alice", age: "22", profession: "designer" },
+    { id: 6, name: "bob", age: "25", profession: "manager" },
+    { id: 7, name: "charlie", age: "23", profession: "developer" }
+  ];
+  let concatenatedArray = arr.concat(newArr);
+  console.log(concatenatedArray);
+}
